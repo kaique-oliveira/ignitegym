7b2f53f802ef6149aced342156b3d5271bb73e6e
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-05-08 16:28:18 -0300

    style: add tokens do theme

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { Loading } from "@components/Loading";
+
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -12,20 +16,13 @@ export default function App() {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle={"light-content"}
         backgroundColor={"transparent"}
         translucent
       />
-      {fontLoaded ? <Text>Hello World</Text> : <View></View>}
-    </View>
+      {fontLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
